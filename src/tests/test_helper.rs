cd55c5d0017067b177aaa25721a6d6224eaf3af3
DZWG <paule22@protonmail.com>
2022-04-08 21:21:20 +0200

    added readme fix error

--- src/tests/test_helper.rs
+++ src/tests/test_helper.rs
@@ -8,14 +8,14 @@ mod test_helper {
     #[tokio::test]
     async fn test_fetch_success() {
         env::remove_var("SOLANABEACH_TOKEN");
-        let result = helper::fetch("http://example.com".to_string(), None).await.unwrap();
+        let result = helper::fetch("http://example.com".to_string()).await.unwrap();
         assert_eq!(result.status(), StatusCode::OK);
     }
 
     #[tokio::test]
     async fn test_fetch_success_with_env() {
         env::set_var("SOLANABEACH_TOKEN", "supertoken123");
-        let result = helper::fetch("http://example.com".to_string(), None).await.unwrap();
+        let result = helper::fetch("http://example.com".to_string()).await.unwrap();
         assert_eq!(result.status(), StatusCode::OK);
     }
 }
